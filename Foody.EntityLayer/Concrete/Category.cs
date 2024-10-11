62f6c670dec2f757843c5b086ea50219258307e3
Yavuz Özbay <ozbayavz@gmail.com>
2024-10-11 12:36:25 +0200

    Add project files.

---
+++ Foody.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+	public class Category
+	{
+		
+			public int CategoryId { get; set; }
+			public string CategoryName { get; set; }
+			public bool CategoryStatus { get; set; }
+			public List<Product> Products { get; set; }
+		
+	}
+}
